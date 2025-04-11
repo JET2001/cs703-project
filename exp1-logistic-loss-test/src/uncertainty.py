cf6480ce4a8e2fc67826f564992a9465b5d8161a
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2025-04-11 17:41:21 +0800

    removed comments

--- exp1-logistic-loss-test/src/uncertainty.py
+++ exp1-logistic-loss-test/src/uncertainty.py
@@ -64,33 +64,3 @@ class BoxUncertainty(BaseUncertainty):
     
     def get_name(self):
         return "Box"
-
-# class BoxUncertainty(BaseUncertainty):
-    
-#     def __init__(self, dataset: Dataset, params: Dict):
-#         super().__init__(params)
-#         col_name = params.get('col_name')
-#         assert col_name in dataset.X_train.columns
-#         X_train_subset, X_test_subset = dataset.X_train[[col_name]].copy(), dataset.X_test[[col_name]].copy()
-
-#         self.step = params.get('step')
-#         self.low = params.get('low')
-#         self.high = params.get('high')
-#         self.bins = np.arange(self.low, self.high, self.step)
-#         res = []
-#         for data in [X_train_subset, X_test_subset]:
-#             curr = []
-#             for x in data:
-#                 bin_no = bisect.bisect_left(self.bins, x)
-#                 curr.append((self.bins[bin_no-1], self.bins[bin_no]))
-#             res.append(curr)
-#         X_train_transformed = dataset.X_train.copy()
-#         X_test_transformed = dataset.X_test.copy()
-#         X_train_transformed[col_name] = curr[0]
-#         X_test_transformed[col_name] = curr[1]
-#         self.transformed = Dataset(X_train_transformed, X_test_transformed, dataset.y_train, dataset.y_test)
-
-#     @override
-#     def get_transformed_dataset(self):
-#         return self.transformed
-
